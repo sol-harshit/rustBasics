1c08e6674363988f29d45c3cd81736f37ccf31dd
sol-harshit <161072524+sol-harshit@users.noreply.github.com>
2024-04-12 07:58:18 +0000

    enums intro completed

--- custom_types/enums/enums_intro.rs
+++ custom_types/enums/enums_intro.rs
@@ -7,9 +7,40 @@ enum WebEvent {
     // An enum may be either unit-like
     PageLoad, // we don't need to define a struct here explicitly
     PageUnload,
-    // like tuple structs
-    KeyPress(char),
     Paste(String),
     // or like structures
     Click { x: i64, y: i64 },
+    // like tuple structs
+    KeyPress(char, i32),
+}
+
+// a function which takes WebEvent enum as an argument amd 
+// returns nothing
+
+fn inspect(event: WebEvent) { 
+    match event {
+        WebEvent::PageLoad => println!("Page loaded"),
+        WebEvent::PageUnload => println!("Page unloaded"),
+        WebEvent::KeyPress(c, d) => println!("Key pressed '{}' and number of times {}", c, d),
+        WebEvent::Paste(s) => println!("Pasted '{}' ", s),
+        WebEvent::Click {x, y} => {
+            println!("Click at x={}, y={} ", x, y);
+        },
+    }
+}
+
+fn main() {
+    let pressed = WebEvent::KeyPress('x', 0);
+    // `to_owned()` creates an owned `String` from a string slice 
+    let pasted = WebEvent::Paste("my text".to_owned());
+    let click = WebEvent::Click {x: 20, y: 90};
+    let load = WebEvent::PageLoad;
+    let unload = WebEvent::PageUnload;
+
+    inspect(pressed);
+    inspect(pasted);
+    inspect(click);
+    inspect(load);
+    inspect(unload);
+
 }
